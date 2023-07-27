4057c88dc6521076b34d6bf19b72e1877c6b21cb
Kobayashi, Yuuki (BIL-MB IPB) 小林 優己 <yuuki.kobayashi2@brother.co.jp>
2023-07-27 10:00:52 +0900

    'を"に統一

--- auto_GUI/auto_GUI/auto_GUI.py
+++ auto_GUI/auto_GUI/auto_GUI.py
@@ -9,13 +9,13 @@ import ctypes
 global g_lines
 global g_f
 
-FUNC_FILE = 'func.txt'
-OUTPUT_FILE = 'output.txt'
+FUNC_FILE = "func.txt"
+OUTPUT_FILE = "output.txt"
 
 #----------------------------------------------------------------------------------------------------#
 # PC操作
 def auto_GUI(array):
-    buf = array.split(',')
+    buf = array.split(",")
     cmt = buf[0]
     cmd = buf[1]
     arg1 = buf[2]
@@ -40,7 +40,7 @@ def auto_GUI(array):
     elif cmd == "Input":
         pyperclip.copy(arg1)
         time.sleep(0.1)
-        pyautogui.hotkey('ctrl','v')
+        pyautogui.hotkey("ctrl","v")
 
     # 特定キー押下(2つ同時まで可)
     elif cmd == "KeyPress":
@@ -58,10 +58,10 @@ def auto_GUI(array):
 
     # 情報のコピー
     elif cmd == "Copy":
-        pyautogui.hotkey('ctrl','c')
+        pyautogui.hotkey("ctrl","c")
         time.sleep(0.1)
-        s_copy = pyperclip.paste() + ',' + arg1
-        g_f.write(s_copy + '\n')
+        s_copy = pyperclip.paste() + "," + arg1
+        g_f.write(s_copy + "\n")
         print(s_copy)
           
     # ファイルのオープン
@@ -73,7 +73,7 @@ def auto_GUI(array):
         end_flag = False
             
         for func_line in g_lines:
-            func_buf = func_line.split(',')
+            func_buf = func_line.split(",")
             func = func_buf[1]
                     
             if end_flag == False and func == arg1:
@@ -89,17 +89,17 @@ def auto_GUI(array):
 #----------------------------------------------------------------------------------------------------#
 # マウス位置記録
 def record_pos():
-    print('Press Esc key if you want to abort.')
+    print("Press Esc key if you want to abort.")
     
     while True:
         if ctypes.windll.user32.GetAsyncKeyState(0x01) != 0:
             x, y = pyautogui.position()
-            s_pos = 'X: ' + str(x) + ' Y: ' + str(y)
-            g_f.write(s_pos + '\n')
+            s_pos = "X: " + str(x) + " Y: " + str(y)
+            g_f.write(s_pos + "\n")
             print(s_pos)
 
         # 「ESC」キーで中断
-        if keyboard.is_pressed('escape'):
+        if keyboard.is_pressed("escape"):
             break
 
         time.sleep(0.1)
@@ -110,36 +110,36 @@ def record_pos():
 # Main
 try:
     # 出力ファイルの内容を削除
-    g_f = open(OUTPUT_FILE, 'a')
+    g_f = open(OUTPUT_FILE, "a")
     g_f.truncate(0)
 
-    print('Choose an option the following list:')
-    print('\t0 - Start Automatic PC control')
-    print('\t1 - Record click position')
-    option = input('Your option? ')
+    print("Choose an option the following list:")
+    print("\t0 - Start Automatic PC control")
+    print("\t1 - Record click position")
+    option = input("Your option? ")
     if option == "0":
         # 操作ファイル入力
-        file = input('Input scenario file name in the same folder as the app(.txt): ')
-        file = file + '.txt'
+        file = input("Input scenario file name in the same folder as the app(.txt): ")
+        file = file + ".txt"
 
         # 操作ファイル読込
-        f = open(file, 'r', encoding='UTF-8')
+        f = open(file, "r", encoding="UTF-8")
         lines = f.readlines()
         f.close()
         
         # 関数ファイル読込
-        f = open(FUNC_FILE, 'r', encoding='UTF-8')
+        f = open(FUNC_FILE, "r", encoding="UTF-8")
         g_lines = f.readlines()
         f.close()
 
-        print('Press Esc key if you want to abort.')
+        print("Press Esc key if you want to abort.")
         time.sleep(0.5)
         
         for line in lines:
             auto_GUI(line)
 
             # 「ESC」キーで中断
-            if keyboard.is_pressed('escape'):
+            if keyboard.is_pressed("escape"):
                 break
 
     elif option == "1":
@@ -150,7 +150,7 @@ except Exception as e:
     
 finally:
     g_f.close()
-    input('Press Enter key to close the app...')
+    input("Press Enter key to close the app...")
 #----------------------------------------------------------------------------------------------------#
     
 

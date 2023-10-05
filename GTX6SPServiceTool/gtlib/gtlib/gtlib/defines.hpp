d838bdf6ae941184c98e1236873b9011a89879ae
Kobayashi, Yuuki (BIL-MB IPB) 小林 優己 <yuuki.kobayashi2@brother.co.jp>
2023-10-05 11:12:14 +0900

    更新2

--- GTX6SPServiceTool/gtlib/gtlib/gtlib/defines.hpp
+++
@@ -1,6 +0,0 @@
-#define	GET_BYTE(p)			( *(p) )
-#define	GET_WORD(p)			( *(p)<<8 | *(p+1) )
-#define	GET_DWORD(p)		( *(p)<<24 | *(p+1)<<16 | *(p+2)<<8 | *(p+3) )
-#define SET_BYTE(x,n,v)		{ *(x+n) = (BYTE)(v); }
-#define SET_WORD(x,n,v)		{ *(x+n) = (BYTE)(v>>8); *(x+n+1) = (BYTE)(v); }
-#define SET_DWORD(x,n,v)	{ *(x+n) = (BYTE)(v>>24); *(x+n+1) = (BYTE)(v>>16); *(x+n+2) = (BYTE)(v>>8); *(x+n+3) = (BYTE)(v); }
--- GTX6SPServiceTool/gtlib/gtlib/ospal.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifndef __APPLE__
-    #include "win/os.h"
-#else
-    #include "mac/os.h"
-#endif
